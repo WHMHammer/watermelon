8f759c698ea3a88757cf586a731f37f2fa475553
ReL <FantasyReimu@gmail.com>
2019-04-15 00:46:36 -0400

    Set up

---
+++ Suika/Suika/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Suika
+//
+//  Created by LDK on 4/15/19.
+//  Copyright © 2019 LDK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
