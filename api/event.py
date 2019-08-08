2d524e852b797c8004a6c448ae612ba36ceffa5e
WHMHammer <whmhammer@gmail.com>
2019-08-08 01:39:03 -0400

    update

--- api/event.py
+++ api/event.py
@@ -190,31 +190,31 @@ def sign_attendance():
 
     return "{}"
 
-#-------------new---------------------------
+
 @bp.route("/subscribe", methods=("POST",))
 def subscribe_event():
     try:
-        event_id=int(flask.g.form["event_id"])
+        event_id = int(flask.g.form["event_id"])
     except (KeyError,TypeError):
-        return "{}",400
+        return "{}", 400
 
     cur = flask.g.db.cursor()
 
-    #check if user has subscribed before
+    #check whether user is already a member
     cur.execute("""
         SELECT *
         FROM roles
         WHERE event_id = %s AND user_id = %s;
-        """,(event_id,flask.g.user["id"]))
+        """,(event_id, flask.g.user["id"]))
 
     if cur.fetchone() != None:
         return dumps({"err_msg":["You have subscribed this event."]}),403
-    else:
-        cur.execute("""
-            INSERT INTO roles(event_id,user_id)
-            VALUES(%s,%s);
-            """,(event_id,flask.g.user["id"]))
 
-        flask.g.db.commit()
-        return "{}"
-#-------------------------------------------
+    cur.execute("""
+        INSERT INTO roles(event_id,user_id)
+        VALUES(%s,%s);
+        """,(event_id, flask.g.user["id"]))
+
+    flask.g.db.commit()
+
+    return "{}"
